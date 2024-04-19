51cbef88636753fcc0f4ffa66ca97863722880b2
faizas333 <faizaamir312@gmail.com>
2024-04-20 02:24:26 +0500

    this is modeuls

---
+++ index.ts
@@ -0,0 +1,9 @@
+import inquirer from "inquirer"
+
+let answer= await inquirer.prompt([{
+    name:"userName",
+    type:"string",
+    massage:"what is your name?",
+}])
+console.log("welcome!!" +answer.userName)
+
